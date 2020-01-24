09fb1088e5e1b766692cdec544d91f5b8915f04b
bala subramanian <msbalasubramanian7871006105@gmail.com>
2020-01-24 22:15:55 +0530

    Updates

---
+++ precache-manifest.d66468d194e1b81a5c9888c6b2ddc875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7be778e4b4b7b5693a93ba6517959b5",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "cffbb07973adec39452e",
+    "url": "/monster-rolodex/static/css/main.74355dba.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/monster-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cffbb07973adec39452e",
+    "url": "/monster-rolodex/static/js/main.b8b48204.chunk.js"
+  },
+  {
+    "revision": "cd391451e66ac7af83df",
+    "url": "/monster-rolodex/static/js/runtime-main.1356d2e6.js"
+  }
+]);
